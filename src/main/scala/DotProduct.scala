903d4bb0e64d62f869d0405761ca7eec30bbb525
alfiehub <alfiehub@users.noreply.github.com>
2017-10-14 18:33:54 +0200

    rewrite dotproduct, need to implement 1 bit. Add simple ROM module

--- src/main/scala/DotProduct.scala
+++ src/main/scala/DotProduct.scala
@@ -2,47 +2,34 @@ package rosetta
 
 import Chisel._
 
-class DotProduct(arr_size: Int, input_width: Int, output_width: Int) extends RosettaAccelerator {
-    def XNOR(a: UInt, b: UInt):UInt = {
+class DotProduct(input_size: Int, input_width: Int) extends RosettaAccelerator {
+    def XNOR(a: UInt, b: UInt): UInt = {
         ~(a ^ b)
     }
-    def expandInt(a: UInt): UInt = {
-        UInt((a*UInt(2))-UInt(1), 2)
+
+    // This function should convert 0 / 1 -> -1 / 1
+    def expandInt(i: UInt): SInt = {
+      i.toSInt * SInt(2) - SInt(1)
     }
 
     val numMemPorts = 0
     val io = new RosettaAcceleratorIF(numMemPorts){
-        val vec_1 = Vec.fill(arr_size){UInt(INPUT, input_width)}
-        val vec_2 = Vec.fill(arr_size){UInt(INPUT, input_width)}
+        val vec_1 = Vec.fill(input_size){UInt(INPUT, input_width)}
+        val vec_2 = Vec.fill(input_size){UInt(INPUT, input_width)}
 
-        val data_out = UInt(OUTPUT, output_width)
+        // Calculate output_width based on input_size and input_width
+        val data_out = UInt(OUTPUT, math.ceil(math.log(input_size) / math.log(2)).toInt + input_width + 1)
     }
 
-
-    val outputArray = Vec.fill(arr_size){UInt(width=output_width)}
-    if(input_width == 1) {
-        for(i <- 0 to arr_size-1){
-            outputArray(i) := expandInt(XNOR(io.vec_1(i), io.vec_2(i)))      // Bitwise XNOR
-        }
-    } else {
-        for(i <- 0 to arr_size-1){
-            outputArray(i) := io.vec_1(i) * io.vec_2(i)
-        }
-    }
-    
-    val sum = Module(new Sum(arr_size, output_width)).io
-
-    sum.nums := outputArray
-
-    io.data_out := sum.data_out
+    io.data_out := (io.vec_1 zip io.vec_2).map{case (i1: UInt, i2: UInt) => i1*i2}.reduceLeft(_ + _)
 }
 
 class DotProductTests(c: DotProduct) extends Tester(c) {
-    val vec_1 = Array[BigInt](0,1,1,0)
-    val vec_2 = Array[BigInt](1,1,0,0)
+    val vec_1 = Array[BigInt](2,1,1,1)
+    val vec_2 = Array[BigInt](2,2,0,1)
 
     poke(c.io.vec_1, vec_1)
     poke(c.io.vec_2, vec_2)
-    peek(c.outputArray)
-    peek(c.io.data_out)
+    expect(c.io.data_out, (vec_1 zip vec_2).map{case (i1, i2) => i1*i2}.reduceLeft(_ + _))
+    expect(c.io.data_out, 7)
 }
