eb0b358117db71ad3eacf7a8f15e610efe409e11
alfiehub <alfiehub@users.noreply.github.com>
2017-10-24 18:34:07 +0200

    Add comments describing the tentative plan for the convolution component

--- src/main/scala/ConvolutionLayer.scala
+++ src/main/scala/ConvolutionLayer.scala
@@ -13,18 +13,39 @@ class ConvolutionLayer(input_width: Int, windows: Int, filters: Int) extends Ros
         val output = Vec.fill(windows * filters){Bits(OUTPUT, math.ceil(math.log(window_size) / math.log(2)).toInt + input_width + 1)}
     }
 
+    // Load weights from file
+    // weights.length == 20
+
     val weights = Vec(
-      scala.io.Source.fromInputStream(this.getClass.getResourceAsStream("/weights/convolution1.txt")).getLines // This looks bad as shit, but scala 2.11.x doesnt have nice support for this
-      .map(line => line.split(" ").map(f => SInt(f.toFloat.toInt, 2))) // How many bits for a brotha? Gotta do toFloat.toInt because of the number format. Could/should be changed
+      // This looks bad as shit, but scala 2.11.x doesnt have nice support for this
+      scala.io.Source.fromInputStream(this.getClass.getResourceAsStream("/weights/convolution1.txt")).getLines
+
+      // How many bits for a brotha? Gotta do toFloat.toInt because of the number format. Could/should be changed w
+      .map(line => line.split(" ").map(f => SInt(f.toFloat.toInt, 2)))
+
       .map(filter => Vec(filter)).toArray
     )
 
-    for (w <- 0 until windows) {
-        for (f <- 0 until filters) {
-          printf(w)
-          printf(f)
+    // Apply filters to all windows given as input
+    // Wait until the previous component states that the data is ready
+    for (f <- 0 until filters) {
+      for (w <- 0 until windows) {
+          // Apply F filters to all windows given
+          // and hook the result up to output
+          printf(w.toString)
+          printf(f.toString)
         }
+
+        // 1. Increment some counter to keep track of how many filters we're done with
+          // We're done with counter * F filters and we proceed with the next F filters next iteration
+        // 2. Wait until the next component is ready.
+          // This is because we have to wait for the next component of
+          // the pipeline to be done with the current output before we change it
     }
+
+    // Set ready after done with all windows given and all 20 filters
+    // to indicate that we're ready for the next W windows
+    // Proceed to wait for data valid from the previous component
 
 }
 
