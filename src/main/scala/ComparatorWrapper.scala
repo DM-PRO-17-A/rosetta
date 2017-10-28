46ea284ba27595ca18dd99f894cd70d9f772fd58
Sebastian Olafsson <msemse.sebastian@gmail.com>
2017-10-28 16:08:59 +0200

    fixed counter bug, now supports a stream of single values

--- src/main/scala/ComparatorWrapper.scala
+++ src/main/scala/ComparatorWrapper.scala
@@ -8,26 +8,34 @@ class ComparatorWrapper(dataWidth: Int, valuesPerIteration: Int, thresholds: Arr
     // values to be compared
     val input = Vec.fill(valuesPerIteration){UInt(INPUT, dataWidth)}
     // values to be output
-    val output = Vec.fill(valuesPerIteration){UInt(OUTPUT, dataWidth)}
+    val output = Decoupled(Vec.fill(valuesPerIteration){UInt(OUTPUT, dataWidth)})
   }
 
   val t = Vec(thresholds.map(s => UInt(s, dataWidth)))
-  val counter = Reg(init=UInt(0))
+  val counter = Reg(init=UInt(width=dataWidth))
 
+  // sets all values in output to be 0, NECESSARY to avoid error
   for(i <- 0 to valuesPerIteration) {
-    io.output := UInt(0)
+    io.output.bits := UInt(0)
   }
 
+  // creates a new comparator for each element in array 
   for(j <- 0 until valuesPerIteration){
       val in = Module(new Comparator(dataWidth)).io
       in.in0 := io.input(UInt(j))
-      printf("%d\n",t(counter + UInt(j)))
+      printf("value being compared: ")
+      printf("%d\n", io.input(UInt(j)))
+      printf("threshold being compared against: ")
+      printf("%d\n", t(counter + UInt(j)))
       in.in1 := t(counter + UInt(j))
-      io.output(UInt(j)) := in.output
+      io.output.bits(UInt(j)) := in.output
     }
 
+
+  // sets the counter to be in correct position for next iteration
   when(counter === UInt(thresholds.length)) {
     counter := UInt(0)
+    printf("counter was reset")
   } .otherwise {
     counter := counter + UInt(valuesPerIteration)
   }
@@ -37,14 +45,32 @@ class ComparatorWrapper(dataWidth: Int, valuesPerIteration: Int, thresholds: Arr
 }
 
 class ComparatorWrapperTest(c: ComparatorWrapper) extends Tester(c) {
-  val test = Array[BigInt](1, 2)
-  val test2 = Array[BigInt](3, 4)
+  val test = Array[BigInt](1)
+  val test2 = Array[BigInt](2)
+  val test3 = Array[BigInt](3)
+  val test4 = Array[BigInt](4)
   poke(c.io.input, test)
   peek(c.io.output)
+  peek(c.counter)
+  expect(c.io.output.bits(0), 0)
+
   step(1)
   poke(c.io.input, test2)
   peek(c.io.output)
-  //expect(c.io.output, Array(1, 0, 1, 0))
+  peek(c.counter)
+  expect(c.io.output.bits(0), 1)
+
+  step(1)
+  poke(c.io.input, test3)
+  peek(c.io.output)
+  peek(c.counter)
+  expect(c.io.output.bits(0), 0)
+  step(1)
+  poke(c.io.input, test4)
+  peek(c.io.output)
+  peek(c.counter)
+  expect(c.io.output.bits(0), 0)
+
 }
 
 
