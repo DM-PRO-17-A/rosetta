f19e0a393e2e0438d8041c9fa9bfe4aa9aee0775
Christoffer Wilhelm Gran <cwgran@gmail.com>
2017-10-28 16:32:52 +0200

    Fix FCLayer accumulators and counters

--- src/main/scala/FullyConnected.scala
+++ src/main/scala/FullyConnected.scala
@@ -3,44 +3,47 @@ package rosetta
 import Chisel._
 
 // These param names are really bad
-class FullyConnected(kernels: Int, weights: Array[Array[Int]], input_size: Int, weight_size: Int, output_size: Int,data_width: Int) extends RosettaAccelerator {
+class FullyConnected(kernels: Int, weights: Array[Array[Int]], input_size: Int, weight_size: Int, output_size: Int, input_width: Int) extends RosettaAccelerator {
+    val output_width = math.ceil(math.log(input_size * 2 ^ input_width) / math.log(2)).toInt + 1
     val numMemPorts = 0
     val io = new RosettaAcceleratorIF(numMemPorts){
-        val input_data = Vec.fill(input_size){UInt(INPUT, data_width)}
+        val input_data = Vec.fill(input_size){UInt(INPUT, input_width)}
 
-        val output_data = Decoupled(Vec.fill(output_size){UInt(OUTPUT, data_width)})
+        val output_data = Decoupled(Vec.fill(output_size){SInt(OUTPUT, output_width)})
     }
+    io.output_data.valid := Bool(false)
 
-    val w = Vec(weights.map(s => Vec(s.map(t => UInt(t, data_width)))))
+    val w = Vec(weights.map(s => Vec(s.map(t => UInt(t, 1)))))
 
-    val acc = Vec.fill(output_size){Reg(init=UInt(width=data_width))}
+    val acc = Vec.fill(output_size){Reg(init=SInt(width=output_width))}
 
-    val weight_counter = Reg(init=UInt(width=data_width))
-    val input_counter = Reg(init=UInt(width=data_width))
+    val weight_counter = Reg(init=UInt(width=output_width))
+    val output_counter = Reg(init=UInt(width=output_width))
 
     for(k <- 0 until kernels){
-        val dotprod = Module(new DotProduct(input_size, data_width)).io
+        val dotprod = Module(new DotProduct(input_size, input_width)).io
         dotprod.vec_1 := io.input_data
 
-        val w_slice = Vec.fill(input_size){UInt(width=data_width)}
+        val w_slice = Vec.fill(input_size){UInt(width=1)}
         for(i <- 0 until input_size) {
             w_slice(i) := w(k)(weight_counter+UInt(i))
         }
         dotprod.vec_2 := w_slice
 
-        acc(weight_counter + UInt(k)) := acc(input_counter) + dotprod.data_out
+        printf("k: %d, dp: %d\n", UInt(k), dotprod.data_out)
+        acc(output_counter + UInt(k)) := acc(output_counter + UInt(k)) + dotprod.data_out
 
-        // This is pretty ugly
-        when(weight_counter === UInt(weight_size)) {
-            weight_counter := UInt(0)
-            when(input_counter === UInt(output_size)) {
-                input_counter := UInt(0)
-            } .otherwise {
-                input_counter := input_counter + UInt(input_size)
+        output_counter := output_counter + UInt(kernels)
+        when(output_counter === UInt(output_size) - UInt(kernels)) {
+            output_counter := UInt(0)
+
+            weight_counter := weight_counter + UInt(input_size)
+            when(weight_counter === UInt(weight_size) - UInt(input_size)) {
+                weight_counter := UInt(0)
+                io.output_data.valid := Bool(true)
             }
-        } .otherwise {
-            weight_counter := weight_counter + UInt(kernels)
         }
+
     }
 
     for(i <- 0 until output_size) {
@@ -49,17 +52,18 @@ class FullyConnected(kernels: Int, weights: Array[Array[Int]], input_size: Int, 
 }
 
 class FullyConnectedTests(c: FullyConnected) extends Tester(c) {
-    val test_array = Array[BigInt](1,1,1,1)
+    val test_array = Array[BigInt](20, 10, 2, 45)
     val step_size = 2
 
-    for(i <- 0 until 4/step_size){
-        poke(c.io.input_data, test_array.slice(i,step_size))
-        peek(c.acc)
-        peek(c.weight_counter)
-        if(i < 4){
-            step(1)
-        }
-    }
+    // This test is really ugly
+    poke(c.io.input_data, Array[BigInt](20,10))
+    peek(c.acc)
+    step(1)
+    poke(c.io.input_data, Array[BigInt](2, 45))
+    peek(c.acc)
+    step(1)
     peek(c.io.output_data)
-    peek(c.weight_counter)
+    expect(c.io.output_data.bits(0), -33)
+    expect(c.io.output_data.bits(1), 77)
+    expect(c.io.output_data.bits(2), -77)
 }
