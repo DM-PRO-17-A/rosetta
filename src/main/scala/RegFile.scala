d37bebc0ee7750eac406b8ffbda2496f3ac86ad5
Pontius1007 <embrikeinang@msn.com>
2017-11-04 16:01:01 +0100

    Testing to read from regfile

--- src/main/scala/RegFile.scala
+++ src/main/scala/RegFile.scala
@@ -2,20 +2,39 @@ package rosetta
 
 import Chisel._
 import fpgatidbits.regfile._
+import fpgatidbits.ocm._
 
 class RegFileGenerator extends RosettaAccelerator {
+    val dataWidth = 32
+    val queueDepth = 32
+    val vec_fill_size = 4
+
     val numMemPorts = 0
     val idBits = log2Up(16)
     val dataBits = 32
     val io = new RosettaAcceleratorIF(numMemPorts) {
         val regFileIF = new RegFileSlaveIF(idBits, dataBits)
 
+        val queue_input = Flipped(Decoupled(UInt(INPUT, width = dataWidth)))
+        val queue_output = (Decoupled(Vec.fill(vec_fill_size){UInt(OUTPUT, width = dataWidth)}))       //Valid and bits are outputs.count
+        val queue_count = UInt(OUTPUT)
+
     }
+
+    val testQueue = Module(new FPGAQueue(Vec.fill(vec_fill_size){UInt(width = dataWidth)}, entries = queueDepth))
 
     //RegFile(numRegs: Int, idBits: Int, dataBits: Int) - databits = width
     val regFile = Module(new RegFile(1, idBits, dataBits)).io
 
+
     io.regFileIF <> regFile.extIF
+
+    //testQueue.io.enq.bits <> io.regFileIF.cmd.bits.writeData
+
+    io.queue_output <> testQueue.io.deq
+    testQueue.io.count <> io.queue_count
+
+
 
 }
 
@@ -26,10 +45,17 @@ class RegFileTests(c: RegFileGenerator) extends Tester(c){
     poke(regFile.cmd.bits.regID, 1)
     poke(regFile.cmd.bits.read, 0)
     poke(regFile.cmd.bits.write, 1)
-    poke(regFile.cmd.bits.writeData, 666)
+    poke(regFile.cmd.bits.writeData, 4)
     poke(regFile.cmd.valid, 1)
     step(1)
     poke(regFile.cmd.valid, 0)
     step(5) // allow the command to propagate and take effect
-    peek(regFile.cmd.bits)
+    poke(regFile.cmd.bits.read, 1)
+    poke(regFile.cmd.bits.write, 0)
+    poke(regFile.cmd.bits.writeData, 0)
+    poke(regFile.cmd.valid, 1)
+    step(1)
+    poke(regFile.cmd.valid, 0)
+    step(1)
+    peek(regFile.readData)
 }
