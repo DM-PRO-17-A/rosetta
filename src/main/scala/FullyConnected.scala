adf0b76e2e2301aaf6c861a9a625673e5317ac15
Christoffer Wilhelm Gran <cwgran@gmail.com>
2017-11-11 14:37:54 +0100

    Add super simple state macine to FC

--- src/main/scala/FullyConnected.scala
+++ src/main/scala/FullyConnected.scala
@@ -4,8 +4,8 @@ import Chisel._
 
 // These param names are really bad
 class FullyConnected(kernels_path: String, kernels_per_it: Int, input_per_it: Int, input_width: Int) extends RosettaAccelerator {
-    val kernels_length = 256
-    val weights_length = 3072
+    val kernels_length = 2
+    val weights_length = 16
     // Map the kernels / weights to UInt
     val kernels = scala.io.Source.fromInputStream(this.getClass.getResourceAsStream(kernels_path)).getLines.toArray.slice(0, kernels_length)
         .map(kernel =>
@@ -16,14 +16,16 @@ class FullyConnected(kernels_path: String, kernels_per_it: Int, input_per_it: In
 
     val numMemPorts = 0
     val io = new RosettaAcceleratorIF(numMemPorts) {
-        val input_data = Vec.fill(input_per_it){UInt(INPUT, input_width)}
+        val input_data = Flipped(Decoupled(Vec.fill(input_per_it){UInt(INPUT, input_width)}))
 
         val output_data = Decoupled(Vec.fill(kernels_length){SInt(OUTPUT, output_width)})
     }
+    io.input_data.ready := Bool(false)
     io.output_data.valid := Bool(false)
 
 
     val acc = Array.fill(kernels_length){Reg(init=SInt(0, width=output_width))}
+
 
     for (i <- 0 until kernels_length) {
         io.output_data.bits(i) := acc(i)
@@ -36,11 +38,42 @@ class FullyConnected(kernels_path: String, kernels_per_it: Int, input_per_it: In
     val current_weight = Reg(init=UInt(0, width=log2Up(weights_length)))
     val current_kernel = Reg(init=UInt(0, width=log2Up(kernels_length)))
 
+    val im_wait :: frame_wait :: calc :: Nil = Enum(UInt(), 3)
+    val state = Reg(init = im_wait)
+
+    switch(state) {
+        is(im_wait) {
+            io.input_data.ready := Bool(true)
+            when(io.input_data.valid) {
+                // Received first input
+                state := calc
+            }
+        } 
+        is(frame_wait) {
+            io.input_data.ready := Bool(true)
+            when(io.input_data.valid) {
+                // Received next input
+                state := calc
+            }
+        }
+        is(calc) {
+            when(current_weight === UInt(weights_length - input_per_it)){
+                when(current_kernel === UInt(kernels_length - kernels_per_it)){
+                    // Done with image
+                    state := im_wait
+                } .otherwise {
+                    // Done with weights for current kernels
+                    state := frame_wait
+                }
+            }
+        }
+    }
+
     val dot_prods = Array.fill(kernels_per_it){Module(new DotProduct(input_per_it, input_width)).io}
 
     for (k <- 0 until kernels_per_it) {
-        dot_prods(k).vec_1 := io.input_data
-        dot_prods(k).vec_2 := io.input_data
+        dot_prods(k).vec_1 := io.input_data.bits
+        dot_prods(k).vec_2 := io.input_data.bits
     }
 
     val weight_step = current_weight / UInt(input_per_it) * UInt(kernels_length) + current_kernel
@@ -59,14 +92,16 @@ class FullyConnected(kernels_path: String, kernels_per_it: Int, input_per_it: In
       }
     }
 
+    when(state === calc) {
     switch (weight_step % UInt(kernels_length)) {
-      for (k <- 0 until kernels_length by kernels_per_it) {
-        is (UInt(k, width=log2Up(kernels_length))) {
-          for (k_i <- 0 until kernels_per_it) {
-            acc(k + k_i) := acc(k + k_i) + dot_prods(k_i).data_out
+          for (k <- 0 until kernels_length by kernels_per_it) {
+            is (UInt(k, width=log2Up(kernels_length))) {
+              for (k_i <- 0 until kernels_per_it) {
+                acc(k + k_i) := acc(k + k_i) + dot_prods(k_i).data_out
+              }
+            }
           }
         }
-      }
     }
 
     when (current_kernel === UInt(kernels_length - kernels_per_it)) {
@@ -88,18 +123,19 @@ class FullyConnectedTests(c: FullyConnected) extends Tester(c) {
     val input_data = scala.io.Source.fromInputStream(this.getClass.getResourceAsStream("/test_data/fc1input60.txt")).getLines.toArray.head.split(" ").map(s => BigInt(s.toInt))
     val kernels = scala.io.Source.fromInputStream(this.getClass.getResourceAsStream("/test_data/fc1.txt")).getLines.toArray.map(kernel => kernel.split(" ").map(s => s.toInt * 2 -1))
 
-    val input_size = 8
-    val kernels_per_iteration = 4
+    val input_size = 16
+    val kernels_per_iteration = 1
 
     for (image <- 0 until 1) {
         val acc = Array.fill(kernels_per_iteration){0}
-        for (i <- 0 until 3072 by input_size) {
+        for (i <- 0 until 16 by input_size) {
             println(i)
-            poke(c.io.input_data, input_data.slice(i, i + input_size))
+            poke(c.io.input_data.valid, 1)
+            poke(c.io.input_data.bits, input_data.slice(i, i + input_size))
             //println(i, i + input_size)
 
             //println(input_data.slice(i, i+input_size).mkString(", "))
-            for (k <- 0 until 256 by kernels_per_iteration) {
+            for (k <- 0 until 2 by kernels_per_iteration) {
                 step(1)
                 //println(kernels(k).slice(i, i+input_size).mkString(", "))
                 //println((input_data.slice(i, i+input_size) zip kernels(k).slice(i, i+input_size)).map{case (i1: BigInt, i2: Int) => i1*i2}.reduceLeft(_+_))
