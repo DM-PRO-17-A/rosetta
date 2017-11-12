4e501d55cb5da1667714bd8df309cb713bd55e49
Sebastian Olafsson <msemse.sebastian@gmail.com>
2017-11-12 16:14:38 +0100

    sewing runs without errors, but data not tested yet

--- src/main/scala/AutoSimple.scala
+++ src/main/scala/AutoSimple.scala
@@ -1,32 +1,34 @@
-// package rosetta
+package rosetta
 
-// import Chisel._
+import Chisel._
 
-// class AutoSimple(dataWidth: Int, valuesPerIteration: Int, thresholds: Array[Int], kernels: Int, weights1: Array[Array[Int]], weights2: Array[Array[Int]], input_size: Int, weight_size: Int, output_size: Int, input_width: Int) extends RosettaAccelerator {
-//   val numMemPorts = 0
-//   val io = new RosettaAcceleratorIF(numMemPorts) {
-//     // values to be compared
-//     val input = Decoupled(Vec.fill(input_size){SInt(INPUT, dataWidth)})
-//     // values to be output
-//     val output = Decoupled(Vec.fill(input_size){UInt(OUTPUT, dataWidth)})
-//   }
+class AutoSimple(kernels_path: String, kernels_per_it: Int, input_per_it: Int, input_width: Int, dataWidth: Int, valuesPerIteration: Int) extends RosettaAccelerator {
+  val numMemPorts = 0
+  val io = new RosettaAcceleratorIF(numMemPorts) {
+    // values to be compared
+    val input = Flipped(Decoupled(Vec.fill(valuesPerIteration){SInt(INPUT, dataWidth)}))
+    // values to be output
+    val output = Decoupled(Vec.fill(valuesPerIteration){UInt(OUTPUT, dataWidth)})
+  }
 
-//   // sets all values in output to be 0, NECESSARY to avoid error
-//   for(i <- 0 until input_size) {
-//     io.output.bits := UInt(0)
-//   }
+  // sets all values in output to be 0, NECESSARY to avoid error
+  for(i <- 0 until 12) {
+    io.output.bits := UInt(0)
+  }
 
-// 	val FC1 = Module(new FullyConnected(kernels, weights1, input_size, weight_size, output_size, input_width)).io
-// 	FC1.input_data := io.input
-// 	val BT = Module(new ComparatorWrapper(dataWidth, valuesPerIteration, thresholds)).io
-// 	BT.input := FC1.output_data
-// 	val FC2 = Module(new FullyConnected(kernels, weights2, input_size, weight_size, output_size, input_width)).io
-// 	FC2.input_data.bits := BT.output.bits
-// 	io.output := FC2.output_data
-// }
+	val FC1 = Module(new FullyConnected(kernels_path: String, kernels_per_it: Int, input_per_it: Int, input_width: Int)).io
+	FC1.input_data.bits := io.input.bits
 
-// class AutoSimpleTest(c: AutoSimple) extends Tester(c) {
+	val BT = Module(new ComparatorWrapper(dataWidth, valuesPerIteration)).io
+	BT.input.bits := FC1.output_data.bits
+
+	val FC2 = Module(new FullyConnected(kernels_path: String, kernels_per_it: Int, input_per_it: Int, input_width: Int)).io
+	FC2.input_data.bits := BT.output.bits
+	io.output.bits := FC2.output_data.bits
+}
+
+class AutoSimpleTest(c: AutoSimple) extends Tester(c) {
   
-// }
+}
 
 
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,15 +19,14 @@ object ChiselMain {
     //chiselMainTest(args, () => Module(new Mux2())){c => new Mux2Tests(c)}
     //chiselMainTest(args, () => Module(new Comparator(8))){c => new ComparatorTest(c)}
     // chiselMainTest(args, () => Module(new ComparatorWrapper(12, 1))){c => new ComparatorWrapperTest(c)}
-    chiselMainTest(args, () => Module(new AutoTest(12, 4))){c => new AutoTestTest(c)}
+    // chiselMainTest(args, () => Module(new AutoTest(12, 4))){c => new AutoTestTest(c)}
     //chiselMainTest(args, () => Module(new DotProduct(4,2))){c => new DotProductTests(c)}
     //chiselMainTest(args, () => Module(new DotProduct(5, 8))){c => new DotProductTests(c)}
     //chiselMainTest(args, () => Module(new ROM())){c => new ROMTests(c)}
-    // chiselMainTest(args, () => Module(new AutoSimple(8, 4, Array(1, 1, 1, 1), 1, Array(Array(3, 5, 4, 1), Array(4, 4, 5, 1)), 4, 4, 8))){c => new AutoSimpleTest(c)}
+    chiselMainTest(args, () => Module(new AutoSimple("/test_data/fc1.txt", 2, 12, 8, 21, 12))){c => new AutoSimpleTest(c)}
     //chiselMainTest(args, () => Module(new DotProduct(5, 8))){c => new DotProductTests(c)}
     //chiselMainTest(args, () => Module(new ROM())){c => new ROMTests(c)}
     //chiselMainTest(args, () => Module(new FullyConnected("/test_data/fc1.txt", 2, 16, 8))){c => new FullyConnectedTests(c)}
-
   }
 }
 
